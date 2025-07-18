afccab1055ffa0fbc1aa72ccf6dbdbb7eb135ad0
Ebuka <davidachibiri8@gmail.com>
2025-07-18 15:38:41 +0200

    Project Start

---
+++ Assignment.py
@@ -0,0 +1,6 @@
+class Assignment:
+    def _init_(self, name, category, weight, grade):
+        self.name = name
+        self.category = category
+        self.weight = weight
+        self.grade = grade
