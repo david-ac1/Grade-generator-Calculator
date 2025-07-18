d9f3a0c0c59c837ef0829c20345e1a37a05f5dec
Ebuka <davidachibiri8@gmail.com>
2025-07-18 15:46:33 +0200

    Class Definitions

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,18 @@
 class Assignment:
     def _init_(self, name, category, weight, grade):
+        #student name
         self.name = name
+        #formative or summative
         self.category = category
+        #Grade class achievable
         self.weight = weight
+        #Grade class achieved
         self.grade = grade
+
+    def get_weighted_grade(self):
+        #Returns the weighted grade for the assignment
+        return (self.weight * self.grade) / 100
+    
+    def __str__(self):
+        #Returns a string representation of the assignment
+        return f"Assignment(name={self.name}, category={self.category,}, weight={self.weight}%, grade={self.grade})"
